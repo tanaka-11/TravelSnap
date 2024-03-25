e6c52b88836e3ed9511f61cbdac08a8d1575afc0
Marina Tanaka <mtanakasg@gmail.com>
2024-03-25 11:48:41 -0300

    impoertações e ajuste

--- App.js
+++ App.js
@@ -1,16 +1,39 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, Image, StatusBar, StyleSheet, Text, View } from "react-native";
+
+// Acessando todos recursos da biblioteca imagePicker
+import * as ImagePicker from "expo-image-picker";
+
+// Acessando todos recursos da MediaLibrary
+import * as MediaLibrary from "expo-media-library";
+
+// Acessando todos recursos da Sharing
+import * as Sharing from "expo-sharing";
+
+// Importação da biblioteca de mapa e o sub-componente "Marker"
+import MapView, { Marker } from "react-native-maps";
+
+// Importação da biblioteca de localização
+import * as Location from "expo-location";
+import { useEffect, useState } from "react";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>TravelSnap</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <View style={estilos.container}>
+        <Text>TravelSnap</Text>
+
+        <View>
+          <Button title="Tirar Foto" />
+        </View>
+
+        <Image />
+      </View>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
+const estilos = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
