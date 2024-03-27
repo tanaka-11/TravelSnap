e9019095928a8802308aac5523eb8d8b2b19841e
Marina Tanaka <mtanakasg@gmail.com>
2024-03-27 08:42:48 -0300

    Telas novas

---
+++ screens/Captura.js
@@ -0,0 +1,146 @@
+// Captura.js
+import React, { useState, useEffect } from "react";
+import { View, Text, TextInput, Button, Image, StyleSheet } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as MediaLibrary from "expo-media-library";
+import * as Location from "expo-location";
+import MapView, { Marker } from "react-native-maps";
+
+export default function CapturaFotoScreen({ navigation }) {
+  const [foto, setFoto] = useState(null);
+  const [descricao, setDescricao] = useState("");
+  const [minhaLocalizacao, setMinhaLocalizacao] = useState(null);
+  const [localizacao, setLocalizacao] = useState(null);
+
+  useEffect(() => {
+    async function obterLocalizacao() {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.log("Permissão negada");
+        return;
+      }
+      let localizacaoAtual = await Location.getCurrentPositionAsync({});
+      setMinhaLocalizacao(localizacaoAtual);
+    }
+    obterLocalizacao();
+  }, []);
+
+  const capturarFoto = async () => {
+    const imagem = await ImagePicker.launchCameraAsync({
+      allowsEditing: false,
+      aspect: [16, 9],
+      quality: 1,
+    });
+
+    if (!imagem.cancelled) {
+      await MediaLibrary.saveToLibraryAsync(imagem.assets[0].uri);
+      setFoto(imagem.assets[0].uri);
+    }
+  };
+
+  const marcarLocal = () => {
+    if (minhaLocalizacao) {
+      setLocalizacao({
+        latitude: minhaLocalizacao.coords.latitude,
+        longitude: minhaLocalizacao.coords.longitude,
+        latitudeDelta: 0.6,
+        longitudeDelta: 0.6,
+      });
+    } else {
+      console.log("Localização ainda não disponível.");
+    }
+  };
+
+  const salvarInfos = async () => {
+    try {
+      const infos = {
+        localizacao: localizacao,
+        urlFoto: foto,
+        descricao: descricao,
+      };
+      const nomeArquivo = "dados_salvos.json";
+      // Salvar as informações aqui
+      console.log("Informações salvas com sucesso!");
+      navigation.navigate("Detalhes", { infos });
+    } catch (error) {
+      console.log("Erro ao salvar as informações", error.message);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>TravelSnap</Text>
+      {foto ? (
+        <View>
+          <TextInput
+            style={styles.input}
+            placeholder="Digite aqui onde sua foto foi tirada!"
+            onChangeText={(text) => setDescricao(text)}
+          />
+        </View>
+      ) : (
+        <Button title="Tirar Foto" onPress={capturarFoto} />
+      )}
+      {foto && (
+        <View>
+          <Image source={{ uri: foto }} style={styles.fotoCapturada} />
+          <View style={styles.viewMapa}>
+            <MapView
+              style={styles.mapa}
+              region={localizacao ?? regiaoInicial}
+              minZoomLevel={15}
+            >
+              {localizacao && (
+                <Marker
+                  coordinate={localizacao}
+                  title="Local da sua foto!"
+                  pinColor="blue"
+                />
+              )}
+            </MapView>
+          </View>
+          {localizacao ? (
+            <Button title="Salvar momento" onPress={salvarInfos} />
+          ) : (
+            <Button title="Marcar localização da foto" onPress={marcarLocal} />
+          )}
+        </View>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  titulo: {
+    textAlign: "center",
+    fontSize: 20,
+    margin: 10,
+  },
+  input: {
+    borderColor: "#000000",
+    borderWidth: 1,
+    margin: 10,
+    padding: 10,
+    borderRadius: 6,
+  },
+  fotoCapturada: {
+    width: 300,
+    height: 300,
+    margin: 10,
+  },
+  mapa: {
+    width: 300,
+    height: 300,
+  },
+  viewMapa: {
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 20,
+  },
+});
---
+++ screens/Detalhes.js
@@ -0,0 +1,23 @@
+import { View, Text, Image } from "react-native";
+
+// Acessando todos recursos da Sharing
+import * as Sharing from "expo-sharing";
+
+export default function Detalhes({ route }) {
+  // Função para compartilhar imagem de fotos
+  // const compartilarFoto = async () => {
+  //   await Sharing.shareAsync(foto);
+  // };
+
+  const { localizacao, urlFoto, descricao } = route.params;
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Descrição: {descricao}</Text>
+      <Image source={urlFoto} />
+      <Text>
+        Localização: {localizacao.latitude}, {localizacao.longitude}
+      </Text>
+    </View>
+  );
+}
