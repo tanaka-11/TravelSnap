b0954f5268ff8767f69802d7efc0c64acdf503ba
Marina Tanaka <huttenloucher@gmail.com>
2024-03-28 20:37:34 -0300

    Update Detalhes.js

--- screens/Detalhes.js
+++ screens/Detalhes.js
@@ -1,7 +1,20 @@
-import { View, Text, Image, Button, StyleSheet } from "react-native";
+import { View, Text, Image, Button, StyleSheet, Share } from "react-native";
 
-export default function Detalhes({ route, navigation }) {
+export default function Detalhes({ route }) {
   const { urlFoto, descricao, latitude, longitude } = route.params.infos;
+
+  // Função para compartilhar
+  const compartilharDetalhes = async () => {
+    try {
+      const mensagem = `Descrição: ${descricao}\nLatitude: ${latitude}\nLongitude: ${longitude}  `;
+      await Share.share({
+        message: mensagem,
+        url: urlFoto,
+      });
+    } catch (error) {
+      console.error("Erro ao compartilhar:", error.message);
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -10,7 +23,7 @@ export default function Detalhes({ route, navigation }) {
         Localização: {latitude} e {longitude}
       </Text>
       <Image source={{ uri: urlFoto }} style={styles.fotoCapturada} />
-      <Button title="Compartilhar" />
+      <Button title="Compartilhar" onPress={compartilharDetalhes} />
     </View>
   );
 }
